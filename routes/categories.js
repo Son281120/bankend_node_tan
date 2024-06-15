66357a836cc1bceeddfa1e7d09d53bfaca4a60e3
Son281120 <changngoc10x28@gmail.com>
2024-06-15 15:14:57 +0700

    categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,18 +1,10 @@
 import express from "express";
-import Categories from "../models/categories.js";
+import { createCategory, findAllCategories } from "../controller/categories.js"
 
 const router = express.Router()
 
-router.post('/', async (req, res) => {
-  try {
-    const productData = req.body
-    const result = await Categories.create(productData);
-    return res.status(201).json({
-      category: result
-    })
-  } catch (error) {
-    return res.status(500).json(error)
-  }
-})
+router.post('/', createCategory);
+
+router.get("/", findAllCategories);
 
 export default router;
